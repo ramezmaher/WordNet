b8962369e1dc931567970775a0989e275621fc98
Ramez Maher <ramezmaher@hotmail.com>
2020-07-22 21:22:07 +0200

    added class that calculates shortest ancestoral path

---
+++ src/SAP.java
@@ -0,0 +1,173 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+import java.util.TreeMap;
+
+import edu.princeton.cs.algs4.Digraph;
+
+public class SAP {
+	private Digraph graph;
+	private int ShortestLength;
+	private int Ancestor;
+	public SAP(Digraph G) {
+		    if(G == null)
+		    	throw new IllegalArgumentException();
+			this.graph = G;
+			ShortestLength = -1;
+			Ancestor = -1;
+	}
+	public int length(int v, int w){
+		if(v >= graph.V() || v < 0 || w >= graph.V() || w < 0)
+			throw new IllegalArgumentException();
+		Set<Integer> a = new HashSet<Integer>();
+		Set<Integer> b = new HashSet<Integer>();
+		a.add(v);
+		b.add(w);
+		find(a,b);
+		return ShortestLength;
+	}
+	public int ancestor(int v, int w) {
+		if(v >= graph.V() || v < 0 || w >= graph.V() || w < 0)
+			throw new IllegalArgumentException();
+		Set<Integer> a = new HashSet<Integer>();
+		a.add(v);
+		Set<Integer> b = new HashSet<Integer>();
+		b.add(w);
+		find(a,b);
+		return Ancestor;
+	}
+	public int length(Iterable<Integer> v, Iterable<Integer> w) {
+		if(v == null || w == null )
+			throw new IllegalArgumentException();
+		for(Integer i : v)
+			if(i >= graph.V() || i < 0)
+				throw new IllegalArgumentException();
+		for(Integer i : w)
+			if(i >= graph.V() || i < 0)
+				throw new IllegalArgumentException();
+		find(v,w);
+		return ShortestLength;
+	}
+	public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
+		if(v == null || w == null)
+			throw new IllegalArgumentException();
+		for(Integer i : v)
+			if(i >= graph.V() || i < 0)
+				throw new IllegalArgumentException();
+		for(Integer i : w)
+			if(i >= graph.V() || i < 0)
+				throw new IllegalArgumentException();
+		find(v,w);
+		return Ancestor;
+	}
+	private void printMap(Map<Integer,Integer> m,int n) {
+		System.out.println(n);
+		for(Integer i: m.keySet())
+			System.out.println(i + "-->" + m.get(i));
+		System.out.println("-----");
+	}
+	private void find(Iterable<Integer> A,Iterable<Integer> B) {
+		Map<Integer,Map<Integer,Integer>> a = new TreeMap<Integer,Map<Integer,Integer>>();
+		Map<Integer,Map<Integer,Integer>> bMap = new TreeMap<Integer,Map<Integer,Integer>>();
+		Set<Integer> visitedA = new HashSet<Integer>();
+		Set<Integer> visitedB = new HashSet<Integer>();
+		for(Integer i : A) { 
+			a.put(i,new TreeMap<Integer,Integer>());
+			a.get(i).put(i, 0);
+		}
+		for(Integer i : B) { 
+			bMap.put(i,new TreeMap<Integer,Integer>());
+			bMap.get(i).put(i, 0);
+		}
+		int counter = 0 ;
+		boolean Fa = false , Fb = false ;
+		while(true) {
+			counter++;
+			boolean flagA = false , flagB = false ;
+			for(Integer i : a.keySet()) {
+				for(Integer j: bMap.keySet()) {
+					for(Integer k: bMap.get(j).keySet())
+					if(a.get(i).containsKey(k)) {
+						System.out.println("found");
+						this.Ancestor = k;
+						this.ShortestLength = a.get(i).get(k) + bMap.get(j).get(k) ;
+						return;
+					}
+				}
+			}
+			if(!Fa) {
+			for(Integer i : a.keySet()) {
+				Set<Integer> s = new HashSet<Integer>(a.get(i).keySet());
+				for (Integer j : s) {
+					for(Integer k : graph.adj(j)) {
+						if(!visitedA.contains(k)) {
+							visitedA.add(k);
+							a.get(i).put(k, counter);
+							flagA = true;
+						}
+					}
+				}
+			}
+			if(!flagA) 
+				Fa = true;
+			}
+			if(!Fb) {
+				for(Integer i : bMap.keySet()) {
+					Set<Integer> s = new HashSet<Integer>(bMap.get(i).keySet());
+					for (Integer j : s) {
+						for(Integer k : graph.adj(j)) {
+							if(!visitedB.contains(k)) {
+								visitedB.add(k);
+								bMap.get(i).put(k, counter);
+								flagB = true;
+							}
+						}
+					}
+				}
+				if(!flagB) 
+					Fb = true;
+			}
+			if(Fb && Fa) {
+				return;
+			}
+			for(Integer i: a.keySet()) {
+				printMap(a.get(i),i);
+				System.out.println("*****");
+			}
+			for(Integer i: bMap.keySet()) {
+				printMap(bMap.get(i),i);
+				System.out.println("*****");
+			}
+		}
+	}
+	public static void main(String[] args) {
+		Digraph G = new Digraph(13);
+		G.addEdge(6, 3);
+		G.addEdge(7, 3);
+		G.addEdge(3, 1);
+		G.addEdge(4, 1);
+		G.addEdge(5, 1);
+		G.addEdge(1, 0);
+		G.addEdge(2, 0);
+		G.addEdge(10, 9);
+		G.addEdge(11, 9);
+		G.addEdge(9, 5);
+		G.addEdge(8, 5);
+	    SAP sap = new SAP(G);
+	    Set<Integer> s1 = new HashSet<Integer>();
+	    s1.add(6);
+	    s1.add(4);
+	    s1.add(10);
+	    Set<Integer> s2 = new HashSet<Integer>();
+	    //s2.add(7);
+	    s2.add(8);
+	    s2.add(2);
+	    int a = sap.ancestor(s1, s2);
+	    int l = sap.length(s1,s2);
+	    System.out.println(a+"  "+l);
+	}
+}
--- src/WordNet.java
+++ src/WordNet.java
@@ -1,20 +1,128 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+
+import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.In;
 
 public class WordNet {
-	
+	private Map<Integer,String> MySynset;
+	private Digraph wordGraph;
+	private int root;
 	public WordNet(String synsets, String hypernyms) {
-		
+		if(synsets == null || hypernyms == null)
+			throw new IllegalArgumentException();
+		MySynset = new HashMap<Integer,String>();
+		wordGraph = new Digraph(MySynset.size());
+		SynsetParser(synsets);
+		HypernymsParser(hypernyms);
+		if(!isDAG())
+			throw new IllegalArgumentException();
+	}
+	private void SynsetParser (String synset) {
+		In input  = new In(synset);
+		while(input.hasNextLine()) {
+			String[] s = input.readLine().split(",");
+			MySynset.put(Integer.parseInt(s[0]),s[1]);
+		}
+	}
+	private void HypernymsParser (String hypernym) {
+		In input = new In(hypernym);
+		while(input.hasNextLine()) {
+			String[] s = input.readLine().split(",");
+			int id  = Integer.parseInt(s[0]);
+			for(int i=1 ; i < s.length ; i++) {
+				wordGraph.addEdge(id,i);
+			}
+		}
 	}
 	public Iterable<String> nouns(){
-		return null;
+		return MySynset.values();
 	}
 	public boolean isNoun(String word) {
-		return false;
+		if(word == null)
+			throw new IllegalArgumentException();
+		return MySynset.containsValue(word);
 	}
 	public int distance(String nounA, String nounB) {
+		if(nounA == null || nounB == null)
+			throw new IllegalArgumentException();
+		if(!isNoun(nounA) || !isNoun(nounB))
+			throw new IllegalArgumentException();
+		Set<Integer> s = MySynset.keySet();
+		Map<Integer,Integer> distances = new HashMap<Integer,Integer>();
+		int index = 0;
+		for(Integer i :  s)
+			if(MySynset.get(i) == nounA)
+				index = i;
+		Queue<Integer> q = new LinkedList<Integer>();
+		q.add(index);
+		s.clear();
+		distances.put(index, 0);
+		while(!q.isEmpty()){
+			int v = q.poll();
+			if(!s.contains(v)) {
+				s.add(v);
+				if(MySynset.get(v).equals(nounB))
+					return distances.get(v);
+				for(Integer i: wordGraph.adj(v)) {
+					q.add(i);
+					distances.put(i,distances.get(v)+1);
+				}
+			}
+		}
 		return 0;
 	}
 	public String sap(String nounA, String nounB) {
+		if(nounA == null || nounB == null)
+			throw new IllegalArgumentException();
+		if(!isNoun(nounA) || !isNoun(nounB))
+			throw new IllegalArgumentException();
 		return null;
 	}
 	
+	private boolean isDAG() {
+		int count = 0 ;
+		Set<Integer> s = MySynset.keySet();
+		for(Integer i : s) {
+			if(wordGraph.outdegree(i) == 0)
+				{
+				root = i ;
+				count++;
+				}
+		}
+		if(count == 0 || count > 1)
+			return false;
+		return true;
+	}
+	private static void print(Map<Integer,Integer> m,int v) {
+		System.out.println(v);
+	    for (Integer i: m.keySet())
+	    	System.out.println(i + "-->" + m.get(i));
+	    System.out.println("----");
+	}
+		public static void main(String[] args) {
+		Digraph g = new Digraph(13);
+		g.addEdge(0, 1);
+		g.addEdge(0, 5);
+		g.addEdge(5, 4);
+		g.addEdge(4, 3);
+		g.addEdge(4, 2);
+		g.addEdge(3, 2);
+		g.addEdge(2, 0);
+		g.addEdge(6, 0);
+		g.addEdge(6, 4);
+		g.addEdge(6, 9);
+		g.addEdge(6, 8);
+		g.addEdge(8, 6);
+		g.addEdge(7, 6);
+		g.addEdge(7, 9);
+		g.addEdge(9, 10);
+		g.addEdge(9, 11);
+		g.addEdge(10, 12);
+		g.addEdge(12, 9);
+		g.addEdge(11, 12);
+	}
 }
